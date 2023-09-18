af27dee5b735e2dc792c03683cfe9cc9bad47e8c
Don Welch <dwelch@nait.ca>
2023-09-18 08:57:27 -0600

    example of refactoring

--- OOPsSolution/SandBox/Program.cs
+++ OOPsSolution/SandBox/Program.cs
@@ -12,3 +12,49 @@ Console.WriteLine(myHome);
 Console.WriteLine(myHome.ToString());
 Console.WriteLine($"City is {myHome.City}");
 
+//an example of Refactoring
+//Refactor means to reduce and clean your code
+bool flag = false;
+if(myHome.Province.ToLower() == "ab")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "bc")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "sk")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+//refactor to cleaner structure
+switch (myHome.Province.ToLower())
+{
+    case "ab":
+    case "bc":
+    case "sk":
+    case "mn":
+        {
+            flag= true;
+            break;
+        }
+    default:
+        {
+            flag = false;
+            break;
+        }
+}
+
+//refactor to compound if
+if (myHome.Province.ToLower() == "ab" ||
+    myHome.Province.ToLower() == "bc" ||
+    myHome.Province.ToLower() == "sk" ||
+    myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
