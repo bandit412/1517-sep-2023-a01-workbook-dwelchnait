50cf983e96cc86ee117d6c997faf65966a731a4d
Don Welch <dwelch@nait.ca>
2023-09-18 08:35:17 -0600

    present an example of the record datatype and a console app to demonstrate

---
+++ OOPsSolution/OOPsReview/Residence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public record Residence(int Number, string Street, string City,
+                            string Province, string PostalCode)
+    {
+        //this new datatype behaves very much like a class
+        //the fields are listed on the declaration
+        //this is a read only version of a class
+        //when declaring an instance you must pass in the data to be held
+        //you cannot alter the record instance
+        //if you need to alter the data within the instance you must
+        //      create a new instance and pass the data to the new instance
+
+        //OPTIONALLY
+        //you can add your own greedy constructor
+        //you can add methods to this datatype
+
+        public override string ToString()
+        {
+            return $"{Number},{Street},{City},{Province},{PostalCode}";
+        }
+    }
+}
---
+++ OOPsSolution/SandBox/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOPsReview;
+
+Console.WriteLine("Hello, World!");
+
+//demonstration of using a Record datatype
+
+//create an instance
+Residence myHome = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome);
+Console.WriteLine(myHome.ToString());
+Console.WriteLine($"City is {myHome.City}");
+
