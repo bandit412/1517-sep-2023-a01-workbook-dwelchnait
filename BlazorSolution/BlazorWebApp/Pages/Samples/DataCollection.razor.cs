5f31442dbf53e4c18ccdd0f69c67d2630dc84455
Don Welch <dwelch@nait.ca>
2023-10-19 15:43:29 -0600

    writing text to a file (csv)

--- BlazorSolution/BlazorWebApp/Pages/Samples/DataCollection.razor.cs
+++ BlazorSolution/BlazorWebApp/Pages/Samples/DataCollection.razor.cs
@@ -1,4 +1,5 @@
-﻿using OOPsReview; //Employment, SupervisoryLevel, Utilities
+﻿using Microsoft.AspNetCore.Components;
+using OOPsReview; //Employment, SupervisoryLevel, Utilities
 
 
 namespace BlazorWebApp.Pages.Samples
@@ -24,6 +25,11 @@ namespace BlazorWebApp.Pages.Samples
         //Since our version of Employment has private sets for some properties
         //  we COULD NOT directly use the employment instance link to our controls
         Employment employment = new Employment();
+
+        //this property will be used as an injection variable to hold
+        //  the access point to the service I will use in the example
+        [Inject]
+        public IWebHostEnvironment webHostEnvironment { get; set; } = default;
 
         protected override Task OnInitializedAsync()
         {
@@ -71,7 +77,50 @@ namespace BlazorWebApp.Pages.Samples
                                                 employmentLevel,
                                                 employmentStartDate,
                                                 employmentYears);
-                    feedbackMessage =employment.ToString();
+
+                    //write the class data as a string to a csv text file
+                    // required:
+                    // a) know the location of the file (name)
+                    // b) the technique to use in writing to the file
+                    //    there are serveral ways to write to the file
+                    //      a) StreamWriter/StreamReader
+                    //      b) using the System.IO.File methods
+                    //           (handles the streaming of the data)
+
+                    //WARNING: when you use the System.IO.File you MUST use the
+                    //          fully qualified naming to the class method you wish\
+                    //          to use.
+                    //         you can not get by just usinng a reference to the
+                    //          namespace at the top of your code (using System.IO;)
+
+                    //there are a couple of ways to refer to your file and its path
+                    //  i) obtain the root path of your application using an injection
+                    //      service called IWebHostEnvironment via property injection
+                    //  ii) use relative addressing starting a the top of your application
+
+                    //in this example we will demonstrate property onjection
+                    //the method that will be use will return the path from the
+                    //  top of your drive to the top of your application
+                    //append the remainder part of the file path to this result (via concentation)
+
+                    //WARNING: the folder slash for your path can be both a forward or back slash
+                    //              on a PC BUT for an Apple machine, you must use the forwardslash (/)
+                    string csvFilePathName = $@"{webHostEnvironment.ContentRootPath}/Data/Employments.csv";
+
+                    //write the data from the employment instance (ToString) as a line to the csv file
+                    //since the string does not contain a line feed character, we will need to concatenate
+                    //  the character (\n)
+                    //the System.IO.File method will be AppendAllText(string)
+                    // AppendAllText will
+                    //   a) create the file if it does not exist
+                    //   b) opens
+                    //   c) writes the text
+                    //   d) closes
+
+                    string line = $"{employment.ToString()}\n";
+                    System.IO.File.AppendAllText(csvFilePathName, line);
+
+                    feedbackMessage = "Data has be saved to file";
                 }
                 catch(FormatException ex)
                 {
